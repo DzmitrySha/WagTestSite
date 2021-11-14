0a2bb9b714c4276d58f247496a8a24d8e2ff2ebe
Dzmitry Shapavalau <ravenbydev@gmail.com>
2021-11-14 11:16:17 +0300

    created tags in base_tags.py for MENU; created top_menu.html template; added MENU to base.html;

--- home/templatetags/base_tags.py
+++ home/templatetags/base_tags.py
@@ -2,12 +2,14 @@
 from home.models import Footer
 from django import template
 
+from wagtail.core.models import Site
+
 # создаем декоратор
 register = template.Library()
 
-# Cоздаем тег, функцию которая будет брать параметр context и
-# возвращать объект (словарь) footer, содержащий сам запрос request по-умолчанию и
-# данные, которые будем использовать в шаблоне -
+# Cоздаем тег футера - функцию которая будет брать параметр context и
+# возвращать объект (словарь) footer, содержащий сам запрос request по-умолчанию
+# и данные, которые будем использовать в шаблоне -
 # - (первую запись из базы данных от модели Footer)
 # Украшаем нашу тег footer_tag() декоратором, который будет принимать два параметра
 # имя файла шаблона (filename) и контекст (takes_context) устанавливаем в True
@@ -17,4 +19,27 @@ def footer_tag(context):
     return {
         'request': context['request'],
         'footer': Footer.objects.first()
+    }
+
+# создаем теги МЕНЮ на основе меню из bakerydemo
+
+# 1. берем тег get_site_root из bakerydemo, c помощью которого
+# мы можем получить корневую страницу сайта,
+# для этого импортируем также модель Site из wagtail.core.models
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    return Site.find_for_request(context['request']).root_page
+
+
+# Извлекает пункты верхнего меню - непосредственные дочерние элементы родительской страницы.
+# Метод has_menu_children необходим, потому что меню Foundation требует,
+# чтобы класс выпадающего списка применялся к родителю.
+@register.inclusion_tag(filename='tags/top_menu.html', takes_context=True)
+def top_menu(context, parent):
+    menuitems = parent.get_children().live().in_menu()
+    return {
+        'menuitems': menuitems,
+        # требуется для тега pageurl, который мы хотим использовать в этом шаблоне
+        'request': context['request'],
     }
